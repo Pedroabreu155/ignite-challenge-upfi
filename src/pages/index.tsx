9b4f08875f8bfc8d8c929bb1a8b5b66492efd009
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-08-20 23:26:49 -0300

    finished challenge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ export default function Home(): JSX.Element {
     return <Loading />;
   }
 
-  if (isLoading && isError) {
+  if (!isLoading && isError) {
     return <Error />;
   }
 
@@ -67,7 +67,15 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+            mt="6"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
