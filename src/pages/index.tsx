7b760fa4167fcc17230075fbd672dfe1de90b511
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-08-20 22:31:07 -0300

    created upload and save image function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,32 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Image = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
+type GetImagesResponse = {
+  after: string;
+  data: Image[];
+};
+
 export default function Home(): JSX.Element {
+  const fetchImages = async ({
+    pageParam = null,
+  }): Promise<GetImagesResponse> => {
+    const { data } = await api('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+
+    return data;
+  };
+
   const {
     data,
     isLoading,
@@ -16,20 +41,25 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage?.after || null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const formatted = data?.pages.flatMap(imageData => {
+      return imageData.data.flat();
+    });
+
+    return formatted;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isLoading && isError) {
+    return <Error />;
+  }
 
   return (
     <>
